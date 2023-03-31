a412d1b2728600268b6b11317ba846357a2a54a5
Kirell Benzi <kirell.benzi@epfl.ch>
2023-03-31 20:00:26 +0200

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser'; 
+
+ const config = {
+   input: 'index.js',
+   output: {
+     file: 'dist/iwanthue.js',
+     format: 'iife',
+     name: 'IWantHue',
+   },
+
+    plugins: [commonjs(), resolve(), terser()],
+ };
+ 
+ export default config;
